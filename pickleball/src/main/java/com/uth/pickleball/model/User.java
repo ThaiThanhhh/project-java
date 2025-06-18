387b18eca82d00182d48733c05916ef250c31464
Nguyenthlinh <162722526+Nguyenthlinh@users.noreply.github.com>
2025-06-18 18:17:47 +0700

    edit
    +chỉnh thành leaner thay vì student cho hợp ngữ cảnh
    +thêm draw.io để theo dõi thiết kế data desgin

--- pickleball/src/main/java/com/uth/pickleball/model/User.java
+++ pickleball/src/main/java/com/uth/pickleball/model/User.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Column;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
     @Column(name = "full_name", nullable = false)
