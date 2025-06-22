6d7f2677b52a1558ebc4accc5e7bf760386aef38
Nguyenthlinh <162722526+Nguyenthlinh@users.noreply.github.com>
2025-06-22 10:25:36 +0700

    #video_library
    đưa vào dữ liệu trong data

---
+++ pickleball/src/main/java/com/uth/pickleball/controller/VideoLibraryController.java
@@ -0,0 +1,26 @@
+package com.uth.pickleball.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import com.uth.pickleball.model.VideoLibrary;
+import com.uth.pickleball.service.VideoLibraryService;
+import java.util.List;
+
+@Controller
+public class VideoLibraryController {
+
+    private final VideoLibraryService videoLibraryService;
+
+    public VideoLibraryController(VideoLibraryService videoLibraryService) {
+        this.videoLibraryService = videoLibraryService;
+    }
+
+    @GetMapping("/videolibrary")
+    public String showVideoLibrary(Model model) {
+        List<VideoLibrary> videos = videoLibraryService.getAllVideos();
+        model.addAttribute("videos", videos);
+        return "private/features/video_library";
+    }
+
+}
---
+++ pickleball/src/main/java/com/uth/pickleball/repositories/IVideoLibraryRepository.java
@@ -0,0 +1,12 @@
+package com.uth.pickleball.repositories;
+
+import com.uth.pickleball.model.VideoLibrary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface IVideoLibraryRepository extends JpaRepository<VideoLibrary, Long> {
+    List<VideoLibrary> findByCategory(String category);
+    List<VideoLibrary> findAll();
+}
